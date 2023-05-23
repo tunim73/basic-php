40aab4eebedb4238e2d99d7d234b18f1f48c7684
tunim73 <antonio.netto973@gmail.com>
2023-05-22 23:03:56 -0300

    Inicio com Php -variaveis, operadores, ifs, laços, arrays

---
+++ basic.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+    Operadores:
+    - &&, ||, and, or
+    - >= <=, ==, !=
+    %
+    +. - . *, /, %
+*/
+/*
+$idade = "20";
+$n = PHP_EOL;
+
+echo "Hello World !" . PHP_EOL;
+
+if ($idade == 23)
+    echo "idade: $idade $n";
+else if ($idade == 20) {
+    $idade = 10000;
+    echo "idade = 20 --> $idade $n";
+}
+
+$mensagem = $idade < 18 ? 'Você é menor de idade' : 'Você é maior de idade';
+echo $mensagem;
+
+echo "$n";
+
+
+echo "new idade: $idade";
+
+*/
+//----------------------------------------------------------------------------------------
+
+    for ($i = 0; $i<10; $i++){
+
+        $teste = "Hello";
+        echo "i: $i" . PHP_EOL;
+    }
+
+echo "$teste";
+
+/*
+while ($i != "xuxa"){
+    echo "i: $i" . PHP_EOL;
+    if($i == 5)
+        $i="xuxa";
+    else
+        $i++;
+}
+
+echo "i: $i" . PHP_EOL;
+*/
---
+++ lista.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ Arrays no PHP internamente funcionam como um HashTables, então tem uma chave e um valor,
+ sendo a chave por padão é um numero, como em outras linguagens, mas posso alterar para
+ uma string, um cpf, etc.
+
+ */
+
+
+
+/*
+$list_idade = ["hello",25,90];
+
+for ($i = 0; $i<count($list_idade); $i++) {
+    echo $list_idade[$i] . PHP_EOL;
+}
+*/
+
+
+
+/*
+Array associativo
+
+Associação de uma chave a um valor, então no array abaixo
+não posso chmar $conta1[0], por que eu alterei a a chave para uma string específica, mas
+também poderia modificar a chave para uma um cpf em string ou valores numericos
+*/
+$conta1  = [
+    'nome' => "Peralta",
+    'saldo' => -15
+];
+$conta2  = [
+    'nome' => "Amy",
+    'saldo' => 1000
+];
+$conta3  = [
+    'nome' => "Gina",
+    'saldo' => 500
+];
+
+
+$contas = [$conta1, $conta2, $conta3];
+
+
+/*
+for($i = 0; $i<count($contas); $i++){
+    echo "conta: " . $contas[$i]['nome'] . PHP_EOL;
+    //A forma de eu pegar o valor
+}
+*/
+/*
+foreach ($contas as $conta) {
+    echo $conta['saldo'] . PHP_EOL;
+}
+*/
+
+$contasComCpf = [
+    1234 => $conta1,
+    5678 => $conta2,
+    91011 => $conta3
+];
+
+/*
+foreach ($contasComCpf as $cpf => $conta) {
+    //para cada chave de $cpf tal, me passe o  seu valor
+
+    echo $cpf.  " " . $conta['saldo'] . PHP_EOL;
+}
+
+*/
+
+$listaDeNomes = ["Naruto", "sasuke"];
+//adicionando um novo elemnto
+$listaDeNomes[] = "sakura";
+/*
+ ou
+ $listaDeNomes[count($listaDeNomes)] = 'sakura'
+ ou
+ $listaDeNomes[3] = 'sakura', mas de forma estática
+*/
+
+$listaDeNomes[1] = "Ino";
+
+foreach ($listaDeNomes as $number => $li ){
+    echo $li .PHP_EOL;
+}
