675c0a7ea598beeddc7c4eba49e134a48ed015e4
tunim73 <antonio.netto973@gmail.com>
2023-05-23 02:12:02 -0300

    importação de arquivos - include ou require

---
+++ funções.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "Arquivo de funções".PHP_EOL;
+function soma (float $x, float $y):float {
+    echo "Arquivo de funções".PHP_EOL;
+    return $x+$y;
+}
+
---
+++ includeAndRequire.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+Quando eu quero importar funções de um arquivo, a princípio, eu preciso
+importar o arquivo, e tudo que há nele, incluindo executar o que tiver fora de escopos de funções
+*/
+/*
+Para importar, pode importar usando include ou require, a diferença é caso o arquivo não seja encontrado o require da erro de imediato.
+Já o include não. Então se o arquivo a ser importado é de suma importância para o prosseguimento, recomenda-se utilizar o require.
+*/
+/*
+É possóivel, sei lá por quê, que eu queira importar o mesmo arquivo duas vezes, pois não se sabe se deu certo na primeira vez.
+Mas será lançado um erro caso, já tenha sido importado, então use, na segunda vez, require_once, só vai importar, caso não tenha
+sido importado antes.
+*/
+require 'funções.php'; // ou include ('funções.php')
+require_once 'funções.php';
+
+$result = soma(5,5);
+
+echo "result = $result";
